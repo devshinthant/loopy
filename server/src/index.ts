e4b075fc88ae9386818e0976440f2cf9992c47ca
Shinn Thant <94442302+DevShinnThant@users.noreply.github.com>
2025-05-14 22:32:07 +0700

    feat: implement create https server

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import express from "express";
+import https from "https";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "/certs/key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "/certs/cert.pem")),
+};
+
+const server = https.createServer(options, app);
+
+server.listen(3000, () => {
+  console.log("HTTPS Server is running on port 3000");
+});
