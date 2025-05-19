b738b8b059316127425c4bb1f26fc3b170b9aef5
devshinthant <shinthantequi@gmail.com>
2025-05-19 16:10:15 +0700

    feat: implement room class

---
+++ server/src/room/room.ts
@@ -0,0 +1,38 @@
+import * as mediasoup from "mediasoup";
+import { AppData } from "mediasoup/node/lib/types";
+import Peer from "./peer";
+import { Socket } from "socket.io";
+
+type Router = mediasoup.types.Router<AppData> | undefined;
+
+class Room {
+  id: string;
+  password: string;
+  router: Router;
+  peers: Map<string, Peer> | undefined;
+
+  constructor(id: string, password: string, router: Router) {
+    this.id = id;
+    this.password = password;
+    this.router = router;
+    this.peers = new Map();
+  }
+
+  addPeer(socket: Socket) {
+    this.peers?.set(socket.id, new Peer(socket));
+  }
+
+  removePeer(socket: Socket) {
+    this.peers?.delete(socket.id);
+  }
+
+  getPeer(socket: Socket) {
+    return this.peers?.get(socket.id);
+  }
+
+  getPeers() {
+    return this.peers;
+  }
+}
+
+export default Room;
