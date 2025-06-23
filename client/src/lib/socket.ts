73622447f69809113f80bbc61b84ac886fa1020b
devshinthant <shinthantequi@gmail.com>
2025-06-23 13:44:13 +0700

    chore: add env file into the gitignore

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
 
+const server = import.meta.env.VITE_SERVER_URL;
+const socketServer = server + "/mediasoup";
+
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? undefined
+    ? socketServer
     : "http://localhost:4000/mediasoup";
 
 export const socket = io(URL, {
